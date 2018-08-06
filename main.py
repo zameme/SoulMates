4b842ae8b6e99e1649a7e86c068be5af4468151f
Roy Morla <rmorla@csumb.edu>
2018-08-06 22:24:56 +0000

    First commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import webapp2
+import jinja2
+import os
+import random
+
+from google.appengine.api import urlfetch
+import json
+
+the_jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+    
+class (webapp2.RequestHandler):
+    def get(self):
+        about_template = the_jinja_env.get_template('templates/about.html')
+        self.response.write(about_template.render())
+
+class ContactPage(webapp2.RequestHandler):
+    def get(self):
+        about_template = the_jinja_env.get_template('templates/contact.html')
+        self.response.write(about_template.render())
+        
+class AboutPage(webapp2.RequestHandler):
+    def get(self):
+        about_template = the_jinja_env.get_template('templates/contact.html')
+        self.response.write(about_template.render())
+        
+app = webapp2.WSGIApplication([
+    ('/', AboutPage),
+    ('/contact', ContactPage),
+], debug=True)
+
+
+accounts = [
+    {
+        "username": "Roy",
+        "password": "showmeme"
+    },
+    {
+        
+    }
+]
